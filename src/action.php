395367cbd1c1ff817bef75ce8adf9fffeecaa6b0
VolodymyrKladochnyi <vladimir@coderiver.com.ua>
2019-01-15 12:06:01 +0200

    added validation to form

--- src/action.php
+++ src/action.php
@@ -1,12 +1,16 @@
 <?php
- $a = $_POST['firstName'];
- $b = $_POST['lastName']; 
- $res = $a ." ". $b;
+if((isset($_POST['firstName'])&&$_POST['firstName']!="")&&(isset($_POST['lastName'])&&$_POST['lastName']!="")){
+    $a = $_POST['firstName'];
+    $b = $_POST['lastName']; 
+    $res = $a ." ". $b;
 
- $file = file_get_contents('users.json');
-$list = json_decode($file,TRUE); 
-unset($file);
-$list[] = [ $res ];
-file_put_contents('users.json',json_encode($list));
+    $file = file_get_contents('users.json');
+    $list = json_decode($file,TRUE); 
+    unset($file);
+    $list[] = [ $res ];
+    file_put_contents('users.json',json_encode($list));
+}else{
+    
+}
 ?>
 
