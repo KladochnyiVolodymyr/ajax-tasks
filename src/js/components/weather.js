df89b4bb7b1be36b36543721c8783ec9fb5c760d
VolodymyrKladochnyi <vladimir@coderiver.com.ua>
2019-01-22 18:08:30 +0200

    upd

--- src/js/components/weather.js
+++ src/js/components/weather.js
@@ -12,22 +12,33 @@ function geoFindMe() {
       url: 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude,
       success: function(result) {
         console.log(result);
+        
         let locationName = result.name +','+ result.sys.country;
         let temperature = result.main.temp;
         let temperatureDef = '<div><span>' + temperature + '</span>°<a href="#" class="js-toggle-temp">C</a></div>';
+        let weatherImgLink = '<img src="' + result.weather[0].icon + '">';
+        
         $('.weather__location').html(locationName);
         $('.weather__temperature').html(temperatureDef);
+        $('.weather__status').html(result.weather[0].description);
+        $('.weather__icon').html(weatherImgLink);
         $('.js-toggle-temp').click(function() {
           $(this).toggleClass('is-F');
+
           if($(this).hasClass('is-F')) {
+
             $('.js-toggle-temp').html('F');
             let temperatureFar = temperature * (9/5) + 32;
             $('.weather__temperature span').html(temperatureFar);
+
           }else{
+
             $('.js-toggle-temp').html('C');
             $('.weather__temperature span').html(temperature);
+
           }
         });
+
       },
       error: function(data) {
       }
